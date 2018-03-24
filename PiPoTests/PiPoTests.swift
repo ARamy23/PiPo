b4aed120a8c9df1a6b83d433d774bb6b7d6ce850
Ahmed Ramy <Dev.AhmedRamy@iCloud.com>
2018-03-24 02:44:43 +0200

    Initial Commit

---
+++ PiPoTests/PiPoTests.swift
@@ -0,0 +1,36 @@
+//
+//  PiPoTests.swift
+//  PiPoTests
+//
+//  Created by Ahmed Ramy on 3/24/18.
+//  Copyright © 2018 Ahmed Ramy. All rights reserved.
+//
+
+import XCTest
+@testable import PiPo
+
+class PiPoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
